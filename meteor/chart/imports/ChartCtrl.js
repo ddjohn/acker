688b35ba42ebe029e233cc399820b18f3f4aee1d
David Johansson <ddjohn@gmail.com>
2018-04-15 19:45:13 +0200

    lsksks

--- meteor/chart/imports/ChartCtrl.js
+++ meteor/chart/imports/ChartCtrl.js
@@ -6,33 +6,38 @@ import { Items } from './ItemsApi.js';
 
 //-----------------------------------
 
+Template.chart.onCreated(function bodyOnCreated() {
+	console.log("chart::onCreated()");
+	Session.set('serie', 'demon');
+	Meteor.subscribe('items');
+});
+
 function buildChart() {
 	console.log('buildChart()');
 
 	new Chartist.Line('.ct-chart', {
 		labels: Session.get('labels'),
 		series: [ 
-			[1,2,3],
-			[2,2,3,3],
 			Session.get('items'),
-		]
+		],
 	},{
+		low: 0,
 		fullWidth: true,
-		chartPadding: {right: 40}
+		chartPadding: {right: 40},
+		showLine: true,
+		showPoint: false,
+		lineSmooth: Chartist.Interpolation.cardinal({
+			tension: 0.2
+		})
 	});
 };
 
-Template.chart.onCreated(function bodyOnCreated() {
-	console.log("chart::onCreated()");
-	Meteor.subscribe('items');
-});
-
 Template.chart.helpers({
 	items() {
-		Session.set('items', Items.find().fetch().map(x => x.value));
-		Session.set('labels', Items.find().fetch().map(x => x._id));
+		Session.set('items', Items.find({'name':Session.get('serie')}).fetch().map(x => x.value));
+		//Session.set('labels', Items.find().fetch({name:Session.get('serie')}).map(x => x._id));
 		buildChart();
-		return Items.find({});
+		return Items.find({'name':Session.get('serie')});
 	},
 });
 
